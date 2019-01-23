1d97adde024fd37d170f2f9a9f8c326b34719cff
Your Name <you@example.com>
2019-01-23 19:49:53 +0530

    office commit

--- zabbixgetdata.py
+++ zabbixgetdata.py
@@ -1,9 +1,19 @@
 from pyzabbix.api import ZabbixAPI
 
 #variables
-user_name = 'admin'
-password_zab = 'password'
-zab_url = 'http://localhost/zabbix'
+user_name = 'Admin'
+password_zab = '12345678'
+zab_url = 'http://192.168.2.226/zabbix'
 
 zapi = ZabbixAPI(url=zab_url, user=user_name, password=password_zab)
 
+zapi.do_request('trend.get',
+{
+    "output": [
+            "itemid",
+            "clock",
+            "num",
+            "value_min",
+            "value_avg",
+            "value_max",
+})
