8ca5596247f13cd0bc8c38d87e4c3d012299747a
Your Name <you@example.com>
2019-01-24 13:50:50 +0530

    office commit

--- variablegen.py
+++ variablegen.py
@@ -1,5 +1,12 @@
 import time
+import datetime
 
-start_time =int(time.time())
 
-print(start_time)
+today = datetime.date.today()
+unix_today = time.mktime(datetime.datetime.strptime(str(today), "%Y-%m-%d").timetuple())
+yesterday = datetime.date.today() - datetime.timedelta(days=1)
+unix_yesterday = time.mktime(datetime.datetime.strptime(str(yesterday), "%Y-%m-%d").timetuple())
+print(today)
+print(int(unix_today))
+print (yesterday)
+print(int(unix_yesterday))
--- zabbixgetdata.py
+++ zabbixgetdata.py
@@ -2,15 +2,21 @@ from pyzabbix.api import ZabbixAPI
 
 # variable imports
 import cred
-# import reportitems
+import variablegen
 
 
 zapi = ZabbixAPI(url=cred.zab_url, user=cred.user_name, password=cred.password_zab)
 
 test: object = zapi.do_request(method='trend.get', params={
-        "output": ["itemid", "value_max"],
-        "itemids": ["23316"],
-        "limit": "1"})
+        "output": [
+                "itemid",
+                "value_max"
+        ],
+        "itemids": [
+                "23316"
+        ],
+        "limit": "1"
+})
 
 
 
