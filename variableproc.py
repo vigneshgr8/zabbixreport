f23e81cbf414bdf32342e0be94cd4f0a7e326ae0
Your Name <you@example.com>
2019-01-24 15:20:44 +0530

    office commit

--- variableproc.py
+++ variableproc.py
@@ -1,12 +1,20 @@
 import time
 import datetime
+import json
 
+from hurry import filesize
+
+#import zabbixgetdata
 
 today = datetime.date.today()
 unix_today = time.mktime(datetime.datetime.strptime(str(today), "%Y-%m-%d").timetuple())
 yesterday = datetime.date.today() - datetime.timedelta(days=1)
 unix_yesterday = time.mktime(datetime.datetime.strptime(str(yesterday), "%Y-%m-%d").timetuple())
-# print(today)
-# print(int(unix_today))
-# print (yesterday)
-# print(int(unix_yesterday))
+print(today)
+print(int(unix_today))
+print (yesterday)
+print(int(unix_yesterday))
+
+#gotoutput = json.loads(zabbixgetdata.test)
+
+#hrvalue = filesize.iec(gotoutput['value_max'])
--- zabbixgetdata.py
+++ zabbixgetdata.py
@@ -2,12 +2,12 @@ from pyzabbix.api import ZabbixAPI
 
 # variable imports
 import cred
-import variablegen
+import variableproc
 
 
 zapi = ZabbixAPI(url=cred.zab_url, user=cred.user_name, password=cred.password_zab)
 
-test: object = zapi.do_request(method='trend.get', params={
+test = zapi.do_request(method='trend.get', params={
         "output": [
                 "itemid",
                 "value_max"
@@ -16,10 +16,10 @@ test: object = zapi.do_request(method='trend.get', params={
                 "23316"
         ],
         "limit": "1",
-        "time_from": variablegen.unix_yesterday,
-        "time_till": variablegen.unix_today,
+        "time_from": variableproc.unix_yesterday,
+        "time_till": variableproc.unix_today,
 })
 
-
+#print(variablegen.hrvalue)
 
 print(test)
