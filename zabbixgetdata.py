f2c8c94300a20cccf5601d0f09dc6abfc710f531
Your Name <you@example.com>
2019-01-24 11:52:22 +0530

    office commit

--- zabbixgetdata.py
+++ zabbixgetdata.py
@@ -2,11 +2,14 @@ from pyzabbix.api import ZabbixAPI
 
 # variable imports
 import cred
-#import reportitems
+# import reportitems
 
 
 zapi = ZabbixAPI(url=cred.zab_url, user=cred.user_name, password=cred.password_zab)
 
-test = zapi.do_request(method='trend.get', params={"output": ["itemid", "value_max"], "itemids": ["23316"], "limit": "1"})
+test: object = zapi.do_request(method='trend.get', params={
+        "output": ["itemid", "value_max"],
+        "itemids": ["23316"],
+        "limit": "1"})
 
 print(test)
