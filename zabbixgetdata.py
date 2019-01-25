071a55dc529174496871e1f761f1f31106fbb6ad
vigneshgr8 <vigneshngr8@gmail.com>
2019-01-25 11:59:51 +0530

    office commit

--- zabbixgetdata.py
+++ zabbixgetdata.py
@@ -8,21 +8,25 @@ import allfunctions
 
 zapi = ZabbixAPI(url=cred.zab_url, user=cred.user_name, password=cred.password_zab)
 
-test: str = zapi.do_request(method='trend.get', params={
-    'output': [
 
-        'itemid',
-        'value_max'
-    ],
-    'itemids': [
-        '23316'
-    ],
-    'limit': '1',
-    'time_from': variablein.unix_yesterday,
-    'time_till': variablein.unix_today
-})
+def zapi_request(item):
 
-hrvalue = allfunctions.dataspeedconv(test)
+    request = zapi.do_request(method='trend.get', params={
+        'output': [
+
+            'itemid',
+            'value_max'
+        ],
+        'itemids': [
+            item
+        ],
+        'limit': '1',
+        'time_from': variablein.unix_yesterday,
+        'time_till': variablein.unix_today
+    })
+    return request
+
+hrvalue = allfunctions.dataspeedconv(zapi_request(23316))
 print(hrvalue)
 
-print(test)
+print(zapi_request(23316))
